ae87a8105964578db7d7c47d8eb1557a7d80f1c6
David Lindström <davlin@creatorise.com>
2024-05-09 10:23:28 +0200

    Add test run php, js and css

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Moments in Motion</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="script.js"></script>
+</head>
+
+<body>
+	<h1>Moments in Motion</h1>
+	<?php echo 'Hello, World!' ?>
+</body>
+
+</html>
